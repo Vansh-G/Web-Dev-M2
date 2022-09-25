9c2aa05a0b0f6d4c1192f91565857e9aca8431cb
Vansh-G <93340942+Vansh-G@users.noreply.github.com>
2022-09-25 14:28:55 +0530

    Update main.js

--- Q2/main.js
+++ Q2/main.js
@@ -1,4 +1,4 @@
-// Setup
+// Accessing all elements using IDs
 let stringCont = document.getElementById("String");
 let intCont = document.getElementById("Int");
 let sumFuncAns = document.getElementById("sum_ans");
@@ -6,20 +6,20 @@ let DMAns = document.getElementById("Dec_Making_Ans");
 let loopAns = document.getElementById("loopAns");
 
 
-// Variables
+// Defining 2 variables. 1 String another Integer and relaying them back to the html file
 let stringVar = "String is: Mid Term 2";
 stringCont.innerHTML = stringVar;
 let integerVar = 124;
 intCont.innerHTML = "Number is: " + integerVar;
 
-// Sum function
+// Made a function that adds two numbers which we have already provided
 let sumFunc = (num1, num2) => {
   return num1 + num2;
 };
 
 sumFuncAns.innerHTML = sumFunc(15, 58);
 
-// Decision making
+// Made a function that  finds if a number is odd or even
 let num = 55;
 if (num%2 == 0) {
   DMAns.innerHTML = "Even";
@@ -27,7 +27,7 @@ if (num%2 == 0) {
   DMAns.innerHTML = "Odd";
 }
 
-// Loops
+// Made a for loop that gives out the factorial of a number(Here 5)
 let fact=1
 let a = 5
 for (let i = 1; i <= a; i++) {
