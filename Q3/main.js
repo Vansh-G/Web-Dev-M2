70efa051fb88036e11fc8a4bb3f2036fc74a8342
Vansh-G <93340942+Vansh-G@users.noreply.github.com>
2022-09-25 13:09:36 +0530

    Update main.js

--- Q3/main.js
+++ Q3/main.js
@@ -1,19 +1,22 @@
+//Getting container for number
 let numberdiv = document.getElementById("num");
 
-
+//Assigning initial value to the variable number
 let number = 0;
 
-
+// Getting access to the buttons using IDs
 let plus = document.getElementById("Increment button");
 let minus = document.getElementById("Decrement button");
 let reset = document.getElementById("Reset button");
 
 
+// Adding the increment Action to increment button
 plus.addEventListener("click", () => {
     ++number;
     numberdiv.innerHTML = number;
 })
 
+// Adding the decrement action to decrement button
 minus.addEventListener("click", () => {
     if (number > 0){
         --number;
@@ -24,6 +27,7 @@ minus.addEventListener("click", () => {
     }
 })
 
+// Adding the reset acion to reset button
 reset.addEventListener("click", () => {
     number = 0;
     numberdiv.innerHTML = number;
